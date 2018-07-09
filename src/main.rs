19b2337d5c8793fb88d5d92dfb403853cf80db27
Heinz Gies <hgies@wayfair.com>
2018-07-09 12:42:39 +0200

    This doens't work

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+trait Geometry {
+    fn new() -> Self;
+    fn draw();
+}
+
+struct Circle {}
+impl Geometry for Circle {
+    fn draw() {
+        println!("()")
+    }
+    fn new() -> Self {
+        Circle {}
+    }
+}
+struct Square {}
+impl Geometry for Square {
+    fn draw() {
+        println!("[]")
+    }
+    fn new() -> Self {
+        Square {}
+    }
+}
+
+fn new_geo(shape: &str) -> impl Geometry {
+    match shape {
+        "square" => Square::new(),
+        "circle" => Circle::new(),
+        other => panic!("Bad shape: {}", other),
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
